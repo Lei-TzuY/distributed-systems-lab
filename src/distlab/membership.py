bdae443f8c895d3cdb712dd62dcbe984da1582d3
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-08 14:43:01 +0800

    Preserve learner append responses while fencing term adoption

--- src/distlab/membership.py
+++ src/distlab/membership.py
@@ -335,18 +335,21 @@ class ReconfigurableRaftNode(RaftNode):
             self._become_leader(response.term)
 
     def _handle_append_entries_response(self, response: AppendEntriesResponse) -> None:
-        follower_eligible = self.cluster.is_voter(response.follower_id)
-        recipient_eligible = self.cluster.is_voter(self.node_id)
-        if not follower_eligible or not recipient_eligible:
-            self.sim._record(
-                "raft-append-response-rejected",
-                node=self.node_id,
-                follower=response.follower_id,
-                term=response.term,
-                current_term=self.current_term,
-                reason=(
-                    "follower-not-voter" if not follower_eligible else "recipient-not-voter"
-                ),
-            )
-            return
+        if response.term > self.current_term:
+            follower_eligible = self.cluster.is_voter(response.follower_id)
+            recipient_eligible = self.cluster.is_voter(self.node_id)
+            if not follower_eligible or not recipient_eligible:
+                self.sim._record(
+                    "raft-append-response-rejected",
+                    node=self.node_id,
+                    follower=response.follower_id,
+                    term=response.term,
+                    current_term=self.current_term,
+                    reason=(
+                        "follower-not-voter"
+                        if not follower_eligible
+                        else "recipient-not-voter"
+                    ),
+                )
+                return
         super()._handle_append_entries_response(response)
